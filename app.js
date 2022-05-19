1d49ee26bc7b4e18cc4b71e8b4640f4aba7affd4
Juz Bird <daweiwizard@gmail.com>
2022-05-19 18:06:10 +0630

    add layout

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ let food = 'salad';
 
 let eat = (food) => "I am eating " + food;
 
-console.log(eat(food));
+// console.log(eat(food));
+
+let iceCreams = ['chocolate', 'strawberry', 'grapes'];
+
+console.log(iceCream);
